68279b4178be66c80d0ebbd4d9dca474f3323604
heelerdeer <heelerdeer@gmail.com>
2025-04-25 20:30:33 +0800

    correct some errors

--- generated_code/fl417_ga.py
+++ generated_code/fl417_ga.py
@@ -60,7 +60,7 @@ def prepare_dataset(filename):
   # print(node_coords)
 
 # filepath = ['ai_project/smaples_data/ali535.tsp','/content/ai_project/smaples_data/eil51.tsp','/content/ai_project/smaples_data/lin318.tsp']
-filepath = ['/content/ai_project/sample_data/fli417.tsp']
+filepath = ['/content/ai_project/sample_data/fl417.tsp']
 for i in filepath:
   prepare_dataset(i)
   
